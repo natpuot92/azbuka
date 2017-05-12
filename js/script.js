78ef09629e09ba8cca1446386d8e771690d73335
natpuot92 <natpuot1992@gmail.com>
2017-05-12 11:43:44 +0300

    last

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $(window).on("scroll", () => {
     let fromTop = $("body").scrollTop();
-    $('.sticky-header').toggleClass("sticky", (fromTop > 496));
+    $('.sticky-header').toggleClass("sticky", (fromTop > 497));
   });
+
+//window.addEventListener('scroll', function() {
+//  var topLine = document.querySelector('.top-line');
+//  if (window.pageYOffset > 490) {
+//    topLine.classList.add('sticky');
+//  }
+//  if (window.pageYOffset < 490) {
+//    topLine.classList.remove('sticky');
+//  }
+//})
